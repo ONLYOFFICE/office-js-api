302d43c7a06ac12c30eb857c17a85c5261c14490
KirillovIlya <ilya.kirillov@onlyoffice.com>
2024-08-03 17:00:06 +0300

    Fix example for ApiInlineLvlSdt.SetPlaceholderText

--- Examples/Word/ApiInlineLvlSdt/Methods/SetPlaceholderText.js
+++ Examples/Word/ApiInlineLvlSdt/Methods/SetPlaceholderText.js
@@ -1,9 +1,6 @@
 // This example sets the placeholder text to the inline content control.
 var oDocument = Api.GetDocument();
 var oParagraph = oDocument.GetElement(0);
-var oRun = Api.CreateRun();
-oRun.AddText("Add a combo box to the next line (Forms -> Combo Box), copy the macro above (without the first and last two lines) and run it (Plugins -> Macros).");
-oParagraph.AddElement(oRun);
-var aForms = oDocument.GetAllForms();
-oParagraph.RemoveElement(0);
-var sText = aForms[0].SetPlaceholderText("Name");
+var oInlineLvlSdt = Api.CreateInlineLvlSdt();
+oParagraph.AddInlineLvlSdt(oInlineLvlSdt);
+oInlineLvlSdt.SetPlaceholderText("Custom placeholder");
