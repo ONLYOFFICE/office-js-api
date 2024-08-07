19f66533e5e3edbb5dfa4dfb91b6f1bfd9840891
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2024-08-07 12:49:23 +0300

    Update AddCustomFunction.js

--- Examples/Cell/Api/Methods/AddCustomFunction.js
+++ Examples/Cell/Api/Methods/AddCustomFunction.js
@@ -1,6 +1,16 @@
 // This example calculates custom function result.
-Api.AddCustomFunction(function ADD(first, second) {
-    return first + second;
-})
+
+Api.AddCustomFunctionLibrary("LibraryName", function(){
+    /**
+     * Function that returns the argument
+     * @customfunction
+     * @param {any} first First argument.
+     * @returns {any} second Second argument.
+    */
+    Api.AddCustomFunction(function ADD(first, second) {
+        return first + second;
+    });
+});
+
 var oWorksheet = Api.GetActiveSheet();
 oWorksheet.GetRange('A1').SetValue('=ADD(1,2)');
