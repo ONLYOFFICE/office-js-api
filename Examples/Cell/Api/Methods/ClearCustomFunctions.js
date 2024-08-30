aa734478177adba814104f17a266712961fa8fd4
GoshaZotov <Igor.Zotov@onlyoffice.com>
2024-08-30 17:21:47 +0300

    [se] Fixed clear/remove custom functions examples

--- Examples/Cell/Api/Methods/ClearCustomFunctions.js
+++ Examples/Cell/Api/Methods/ClearCustomFunctions.js
@@ -1,10 +1,16 @@
+// This example clear all added custom functions.
+Api.AddCustomFunctionLibrary("LibraryName", function(){
+    /**
+     * Function that returns the argument
+     * @customfunction
+     * @param {any} first First argument.
+     * @returns {any} second Second argument.
+     */
+    Api.AddCustomFunction(function ADD(first, second) {
+        return first + second;
+    });
+});
 
-Api.AddCustomFunction (function add (first, second) {
-    if (second === null) {
-        second = 0;
-    }
-    return first + second;
-})
 var oWorksheet = Api.GetActiveSheet();
 oWorksheet.GetRange("A1").SetValue("=ADD(1, 2)");
 Api.ClearCustomFunctions();
