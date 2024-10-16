bdc3301880cea8cf57da420e99bd7780aea54edb
Sergey Linnik <sergey.linnik@onlyoffice.com>
2024-10-16 15:17:27 +0000

    [de] Fix same key

--- Examples/Word/ApiDocument/Methods/GetAllForms.js
+++ Examples/Word/ApiDocument/Methods/GetAllForms.js
@@ -1,9 +1,9 @@
 // This example shows how to get all existing forms in the document.
 var oDocument = Api.GetDocument();
-var oTextForm = Api.CreateTextForm({"key": "Personal information", "tip": "Enter your first name", "required": true, "placeholder": "First name", "comb": true, "maxCharacters": 10, "cellWidth": 3, "multiLine": false, "autoFit": false});
+var oTextForm = Api.CreateTextForm({"key": "Name", "tip": "Enter your first name", "required": true, "placeholder": "First name", "comb": true, "maxCharacters": 10, "cellWidth": 3, "multiLine": false, "autoFit": false});
 var oParagraph = oDocument.GetElement(0);
 oParagraph.AddElement(oTextForm);
-var oComboBoxForm = Api.CreateComboBoxForm({"key": "Personal information", "tip": "Choose your country", "required": true, "placeholder": "Country", "editable": false, "autoFit": false, "items": ["Latvia", "USA", "UK"]});
+var oComboBoxForm = Api.CreateComboBoxForm({"key": "Country", "tip": "Choose your country", "required": true, "placeholder": "Country", "editable": false, "autoFit": false, "items": ["Latvia", "USA", "UK"]});
 oParagraph.AddLineBreak();
 oParagraph.AddElement(oComboBoxForm);
 var aForms = oDocument.GetAllForms();
