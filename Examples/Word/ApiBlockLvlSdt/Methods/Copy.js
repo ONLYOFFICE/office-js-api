148f3f692ff999bcc19dfc2a4d53505a830926e5
Nikita Khromov <nikita.khromov@onlyoffice.com>
2024-10-23 15:17:58 +0700

    [bu][de] Added new method

---
+++ Examples/Word/ApiBlockLvlSdt/Methods/Copy.js
@@ -0,0 +1,8 @@
+// This example creates a copy of an block level content control.
+let oDocument = Api.GetDocument();
+let oParagraph = oDocument.GetElement(0);
+let oBlockLvlSdt = Api.CreateBlockLvlSdt();
+let oPara = oBlockLvlSdt.GetContent().GetElement(0);
+oPara.AddText("This is an block level content control.");
+let oCopyBlockLvlSdt = oBlockLvlSdt.Copy();
+oDocument.Push(oCopyBlockLvlSdt);
