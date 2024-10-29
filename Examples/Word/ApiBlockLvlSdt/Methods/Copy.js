0c135ffc9f2b28c912387be939cee878f90064b2
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2024-10-29 13:50:09 +0300

    Improve example for ApiBlockLvlSdt.Copy

--- Examples/Word/ApiBlockLvlSdt/Methods/Copy.js
+++ Examples/Word/ApiBlockLvlSdt/Methods/Copy.js
@@ -4,5 +4,6 @@ let oParagraph = oDocument.GetElement(0);
 let oBlockLvlSdt = Api.CreateBlockLvlSdt();
 let oPara = oBlockLvlSdt.GetContent().GetElement(0);
 oPara.AddText("This is an block level content control.");
+oDocument.Push(oBlockLvlSdt);
 let oCopyBlockLvlSdt = oBlockLvlSdt.Copy();
 oDocument.Push(oCopyBlockLvlSdt);
