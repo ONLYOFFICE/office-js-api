56862a11d3b52ac49447560f0d1bca083f25a1c1
dora-g <dildorakhon.ganieva@onlyoffice.com>
2025-02-07 18:00:29 +0500

    Commented ApiChartSeries, ApiColor, ApiComment

--- Examples/Cell/ApiComment/Methods/Delete.js
+++ Examples/Cell/ApiComment/Methods/Delete.js
@@ -1,4 +1,6 @@
 // This example deletes the ApiComment object.
+// How to remove a comment from a range.
+// Get a range add a comment to it and then remove it.
 var oWorksheet = Api.GetActiveSheet();
 oWorksheet.GetRange("A1").SetValue("1");
 var oRange = oWorksheet.GetRange("A1");
--- Examples/Cell/ApiComment/Methods/IsSolved.js
+++ Examples/Cell/ApiComment/Methods/IsSolved.js
@@ -1,4 +1,6 @@
 // This example checks if a comment is solved or not.
+// How to find out whether a comment is resolved.
+// Add a comment resolved status to a range of the worksheet.
 var oWorksheet = Api.GetActiveSheet();
 oWorksheet.GetRange("A1").SetValue("1");
 var oRange = oWorksheet.GetRange("A1");
