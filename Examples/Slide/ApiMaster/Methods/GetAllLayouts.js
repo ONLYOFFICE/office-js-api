977939341606b374b7d57875e60af3f9873efc1a
Eduard Belozertsev <eduard.belozertsev@onlyoffice.com>
2025-04-29 11:57:50 +0700

    Add example for GetAllLayouts of ApiMaster

--- Examples/Slide/ApiMaster/Methods/GetAllLayouts.js
+++ Examples/Slide/ApiMaster/Methods/GetAllLayouts.js
@@ -1,5 +1,25 @@
 // This example shows how to get all layouts of the specified slide master
+
 const oPresentation = Api.GetPresentation();
-const aSlideMasters = oPresentation.GetAllSlideMasters();
-const oChosenMaster = aSlideMasters[0];
-const aLayouts = oChosenMaster.GetAllLayouts();
+const oSlide = oPresentation.GetSlideByIndex(0);
+const oMaster = oPresentation.GetMaster(0);
+
+oSlide.RemoveAllObjects();
+oMaster.RemoveLayout(0, oMaster.GetAllLayouts().length);
+
+const nLayoutsCount = 3;
+for (let i = 0; i < nLayoutsCount; i++) {
+	const oLayout = Api.CreateLayout();
+	oMaster.AddLayout(0, oLayout);
+}
+
+const oLabelFill = Api.CreateSolidFill(Api.CreateRGBColor(210, 230, 250));
+const oLabelStroke = Api.CreateStroke(0, Api.CreateNoFill());
+const oLabel = Api.CreateShape('rect', 60 * 36000, 20 * 36000, oLabelFill, oLabelStroke);
+oLabel.SetPosition(0, 3 * 36000);
+oSlide.AddObject(oLabel);
+
+const aLayouts = oMaster.GetAllLayouts();
+const oDocContent = oLabel.GetDocContent();
+const oParagraph = oDocContent.GetElement(0);
+oParagraph.AddText('Number of master slide layouts: ' + aLayouts.length);
