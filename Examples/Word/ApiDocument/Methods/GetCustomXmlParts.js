4a7c324c2783c117d74385dcca92dd44180b0005
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2025-05-21 22:33:49 +0300

    Fix GetCustomXmlParts example

--- Examples/Word/ApiDocument/Methods/GetCustomXmlParts.js
+++ Examples/Word/ApiDocument/Methods/GetCustomXmlParts.js
@@ -1,9 +1,9 @@
 // This example shows how to get the custom XML manager, add a new XML part, and retrieve it by its ID.
 let doc = Api.GetDocument();
 let xmlManager = doc.GetCustomXmlParts();
-let xmlText = '<content><text>Example XML</text></content>';
-let xml = xmlManager.Add(xmlText);
-let xmlId = xml.id;
+let xmlText = "<content><text>Example XML</text></content>";
+let xmlPart = xmlManager.Add(xmlText);
+let xmlId = xmlPart.GetId();
 let retrievedXml = xmlManager.GetById(xmlId);
 let infoParagraph = Api.CreateParagraph();
 infoParagraph.AddText("Retrieved XML: " + retrievedXml.GetXml());
