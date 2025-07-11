b1ac6fce22ded1ab4e5f4973218fbc62c5b8ef38
GoshaZotov <Igor.Zotov@onlyoffice.com>
2025-07-11 16:06:14 +0300

    [se] Added new example for custom function

---
+++ Examples/Cell/Api/Methods/AddCustomFunctionLibrary.js
@@ -0,0 +1,23 @@
+// This example calculates custom function result.
+
+// How to add custom function library.
+
+// How to use custom function.
+
+// How to add cell values using custom function library.
+
+Api.AddCustomFunctionLibrary("LibraryName", function(){
+	/**
+	 * Function that returns the argument
+	 * @customfunction
+	 * @param {number} first First argument.
+	 * @param {number} second Second argument.
+	 * @returns {number} The sum of the numbers.
+	 */
+	Api.AddCustomFunction(function ADD(first, second) {
+		return first + second;
+	});
+});
+
+let worksheet = Api.GetActiveSheet();
+worksheet.GetRange('A1').SetValue('=ADD(1,2)');
