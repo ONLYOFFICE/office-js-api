d3bbdc7d5451506b22188034faf40651937171b4
Eduard Belozertsev <eduard.belozertsev@onlyoffice.com>
2025-07-15 09:54:55 +0700

    Fix example for getting start/end pages - add ForceRecalculate

--- Examples/Word/ApiRange/Methods/GetEndPage.js
+++ Examples/Word/ApiRange/Methods/GetEndPage.js
@@ -12,19 +12,9 @@ for (let i = 0; i <= 100; i++) {
 const range = doc.GetRange(2367, 2413);
 range.SetBold(true);
 
+doc.ForceRecalculate();
 const startPage = range.GetStartPage();
 const endPage = range.GetEndPage();
 const newParagraph = Api.CreateParagraph();
 newParagraph.AddText('Bold text starts on page ' + startPage + ' and ends on page ' + endPage + '.');
 doc.Push(newParagraph);
-
-/*
-
-// There is a bug - range.GetStartPage() returns 0,
-// but it works correctly when page is redrawn.
-setTimeout(() => {
-	console.log('Actual start page: ' + range.GetStartPage())
-	console.log('Actual end page: ' + range.GetEndPage())
-}, 1000)
-
-*/
