c725e26f6a7206e4c7a0962abfac8f3ea6452c8c
EvgeniyIgol <evgeny.igol@onlyoffice.com>
2025-09-09 09:56:33 +0300

    Fix examples of AddMathEquation

--- Examples/Slide/ApiPresentation/Methods/AddMathEquation.js
+++ Examples/Slide/ApiPresentation/Methods/AddMathEquation.js
@@ -1,5 +1,31 @@
-// This example adds a math equation to the document.
+// This example adds math equations to the document in three different formats: LaTeX, Unicode, and MathML.
 
-const equation = "e^x = 1 + x + \\frac{x^2}{2} + \\frac{x^3}{6} + \\cdots = \\sum_{n\\geq 0} \\frac{x^n}{n!}";
+// How to add math equations in different formats.
+
+// Insert math equations using LaTeX, Unicode, and MathML formats.
+
 const presentation = Api.GetPresentation();
-presentation.AddMathEquation(equation, "latex");
+
+presentation.AddMathEquation("e^x = 1 + x + \\frac{x^2}{2} + \\frac{x^3}{6} + \\cdots = \\sum_{n\\geq 0} \\frac{x^n}{n!}", "latex");
+
+presentation.AddMathEquation("e^(iπ) + 1 = 0", "unicode");
+
+let xml = `<math>
+		<mrow>
+			<msup>
+				<mi>a</mi>
+				<mn>2</mn>
+			</msup>
+			<mo>+</mo>
+			<msup>
+				<mi>b</mi>
+				<mn>2</mn>
+			</msup>
+			<mo>=</mo>
+			<msup>
+				<mi>c</mi>
+				<mn>2</mn>
+			</msup>
+		</mrow>
+</math>`;
+presentation.AddMathEquation(xml, "mathml");
