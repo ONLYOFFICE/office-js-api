9588863cec20243139bd7c3268ae111ed446ab46
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2026-01-12 12:26:28 +0700

    [pdf] Added examples for text annot

---
+++ Examples/PDF/ApiTextAnnotation/GetClassType.js
@@ -0,0 +1,8 @@
+// This example gets class type of text annotation.
+let doc = Api.GetDocument();
+let textAnnot = Api.CreateTextAnnot([10, 10, 20, 20]);
+let classType = textAnnot.GetClassType();
+textAnnot.SetContents(classType);
+let page = doc.GetPage(0);
+page.AddAnnot(textAnnot);
+console.log(`Annot class type is: ${classType}`);
