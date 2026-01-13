fea57bb9222f6e24ef10fce918da535b1f24745a
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2026-01-13 18:18:15 +0700

    [pdf][bu] Added polyline annot methods examples

---
+++ Examples/PDF/ApiPolylineAnnotation/GetEndStyle.js
@@ -0,0 +1,8 @@
+// This example gets line end style of polyline annotation.
+let doc = Api.GetDocument();
+let vertices = [{x: 100, y: 10}, {x: 150, y: 100}, {x: 50, y: 100}];
+let polylineAnnot = Api.CreatePolyLineAnnot([40, 5, 160, 110], vertices);
+polylineAnnot.SetBorderColor(Api.CreateRGBColor(255, 0, 0));
+let page = doc.GetPage(0);
+page.AddAnnot(polylineAnnot);
+console.log(`Default line end style is: ${polylineAnnot.GetEndStyle()}`);
---
+++ Examples/PDF/ApiPolylineAnnotation/SetStartStyle.js
@@ -0,0 +1,9 @@
+// This example sets line start style to polyline annotation.
+let doc = Api.GetDocument();
+let vertices = [{x: 100, y: 10}, {x: 150, y: 100}, {x: 50, y: 100}];
+let polylineAnnot = Api.CreatePolyLineAnnot([40, 5, 160, 110], vertices);
+polylineAnnot.SetBorderColor(Api.CreateRGBColor(255, 0, 0));
+polylineAnnot.SetStartStyle("circle");
+let page = doc.GetPage(0);
+page.AddAnnot(polylineAnnot);
+console.log(`We set line start style to: ${polylineAnnot.GetStartStyle()}`);
