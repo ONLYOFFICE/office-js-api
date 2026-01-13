f6f3cda07415e6173c3cbb9e3689dbd654d767b7
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2026-01-13 15:38:31 +0700

    [pdf][bu] Added polygon annot methods examples

---
+++ Examples/PDF/ApiPolygonAnnotation/GetClassType.js
@@ -0,0 +1,8 @@
+// This example gets class type of polygon annotation.
+let doc = Api.GetDocument();
+let vertices = [{x: 100, y: 10}, {x: 150, y: 100}, {x: 50, y: 100}];
+let polygonAnnot = Api.CreatePolygonAnnot([40, 5, 160, 110], vertices);
+polygonAnnot.SetBorderColor(Api.CreateRGBColor(255, 0, 0));
+let page = doc.GetPage(0);
+page.AddAnnot(polygonAnnot);
+console.log(`Annot class type is: ${polygonAnnot.GetClassType()}`);
