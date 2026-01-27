d99eef4eca501acc9abe7aab20fe6b7498f7e7be
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2026-01-27 13:51:45 +0700

    [pdf] Added ApiTableRow examples

---
+++ Examples/PDF/ApiTableRow/GetCellsCount.js
@@ -0,0 +1,17 @@
+// How to return a number of cells from the row.
+
+// Create a table, count its cells and display the number in the page.
+
+const doc = Api.GetDocument();
+const page = doc.GetPage(0);
+
+const table = Api.CreateTable(2, 4);
+const row = table.GetRow(0);
+const cellsCount = row.GetCellsCount();
+const cell = row.GetCell(0);
+const content = cell.GetContent();
+const paragraph = Api.CreateParagraph();
+paragraph.AddText("The number of cells in the row: " + cellsCount);
+content.Push(paragraph);
+
+page.AddObject(table);
