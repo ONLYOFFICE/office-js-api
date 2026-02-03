abf63f9d4d36be45c43e9bd64929e1b54f49a1dc
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2026-02-03 11:57:54 +0300

    Add examples for SetLock/GetLock methods for forms

---
+++ Examples/Word/ApiFormBase/Methods/GetLock.js
@@ -0,0 +1,15 @@
+// This example checks if the current form is locked.
+
+// How to know whether a form is locked or not.
+
+// Display a boolean representing whether a form is locked.
+
+let doc = Api.GetDocument();
+let textForm = Api.CreateTextForm({"key": "Personal information", "tip": "Enter your first name", "required": true, "placeholder": "First name", "comb": true, "maxCharacters": 10, "cellWidth": 3, "multiLine": false, "autoFit": false});
+let paragraph = doc.GetElement(0);
+paragraph.AddElement(textForm);
+textForm.SetLock(true);
+let locked = textForm.GetLock();
+paragraph = Api.CreateParagraph();
+paragraph.AddText("The first form from this document is locked: " + locked);
+doc.Push(paragraph);
---
+++ Examples/Word/ApiFormBase/Methods/SetLock.js
@@ -0,0 +1,15 @@
+// This example specifies if the current form should be locked.
+
+// How to lock a form.
+
+// Make sure that the form cannot be edited.
+
+let doc = Api.GetDocument();
+let textForm = Api.CreateTextForm({"key": "Personal information", "tip": "Enter your first name", "required": true, "placeholder": "First name", "comb": true, "maxCharacters": 10, "cellWidth": 3, "multiLine": false, "autoFit": false});
+let paragraph = doc.GetElement(0);
+paragraph.AddElement(textForm);
+textForm.SetLock(true);
+let locked = textForm.GetLock();
+paragraph = Api.CreateParagraph();
+paragraph.AddText("The first form from this document is locked: " + locked);
+doc.Push(paragraph);
