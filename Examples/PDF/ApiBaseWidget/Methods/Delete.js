8b352852c3a07356bb07c9f41789078d69bdf92c
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2026-02-05 13:19:40 +0700

    [pdf] Added new Delete methods

--- Examples/PDF/ApiBaseWidget/Methods/Delete.js
+++ Examples/PDF/ApiBaseWidget/Methods/Delete.js
@@ -1,4 +1,4 @@
-// This example removes widget from parent field.
+// This example removes the widget from parent field.
 let doc = Api.GetDocument();
 let page = doc.GetPage(0);
 let textField = Api.CreateTextField([10, 10, 160, 30]);
@@ -7,4 +7,4 @@ page.AddObject(textField);
 textField.SetValue('Example text');
 textField.AddWidget(0, [10, 40, 160, 60]);
 let widgets = textField.GetAllWidgets();
-widgets[0].Remove();
+widgets[0].Delete();
