c253057da0a90ac975983a7d723549b93ed598f8
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2026-02-17 19:20:42 +0300

    Fix typo editor->Api

--- Examples/Slide/ApiSlide/Methods/GetAllTables.js
+++ Examples/Slide/ApiSlide/Methods/GetAllTables.js
@@ -4,7 +4,7 @@ const presentation = Api.GetPresentation();
 const slide = presentation.GetSlideByIndex(0);
 slide.RemoveAllObjects();
 
-const table = editor.CreateTable(3, 2);
+const table = Api.CreateTable(3, 2);
 table.GetRow(0).GetCell(0).GetContent().GetCurrentParagraph().AddText('US');
 table.GetRow(0).GetCell(1).GetContent().GetCurrentParagraph().AddText('CH');
 table.GetRow(0).GetCell(2).GetContent().GetCurrentParagraph().AddText('Others');
