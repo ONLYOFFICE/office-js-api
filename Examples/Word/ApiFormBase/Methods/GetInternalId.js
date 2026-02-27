66205557a3daaf778ce827c54324e797c89b74e1
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2026-02-27 15:07:33 +0300

    Add missing examples for form-api classes

---
+++ Examples/Word/ApiFormBase/Methods/GetInternalId.js
@@ -0,0 +1,14 @@
+// This example shows how to get the internal id of a form.
+
+// How to get an internal id of a text form.
+
+// Retrieve the internal id of a created text form and display it.
+
+let doc = Api.GetDocument();
+let textForm = Api.CreateTextForm({"key": "Personal information", "tip": "Enter your first name", "required": true, "placeholder": "First name", "comb": true, "maxCharacters": 10, "cellWidth": 3, "multiLine": false, "autoFit": false});
+let paragraph = doc.GetElement(0);
+paragraph.AddElement(textForm);
+let internalId = textForm.GetInternalId();
+paragraph = Api.CreateParagraph();
+paragraph.AddText("Internal id: " + internalId);
+doc.Push(paragraph);
---
+++ Examples/Word/ApiPictureForm/Methods/GetBorderColor.js
@@ -0,0 +1,16 @@
+// This example shows how to get the border color of the current form.
+
+// How to get the border color of a picture form.
+
+// Retrieve the border color of a created picture form and display it.
+
+let doc = Api.GetDocument();
+let pictureForm = Api.CreatePictureForm({"key": "Personal information", "tip": "Upload your photo", "required": true, "placeholder": "Photo", "scaleFlag": "tooBig", "lockAspectRatio": true, "respectBorders": false, "shiftX": 50, "shiftY": 50});
+let paragraph = doc.GetElement(0);
+paragraph.AddElement(pictureForm);
+pictureForm.SetImage("https://static.onlyoffice.com/assets/docs/samples/img/onlyoffice_logo.png", Api.MillimetersToEmus(70), Api.MillimetersToEmus(80));
+pictureForm.SetBorderColor(Api.RGB(255, 111, 61));
+let borderColor = pictureForm.GetBorderColor();
+paragraph = Api.CreateParagraph();
+paragraph.AddText("Border color (RGB): (" + borderColor.r + ", " + borderColor.g + ", " + borderColor.b + ")");
+doc.Push(paragraph);
