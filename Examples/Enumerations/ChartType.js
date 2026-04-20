16d84c3c11fc6f735943227397602a474179b51d
Alexander Trofimov <alexander.trofimov@onlyoffice.com>
2026-04-20 13:53:21 +0300

    fix: remove duplicate chart example and fix LineDash comment
    Remove spreadsheet chart example from ChartType.js (keep Word only).
    Replace "How to" with imperative form and add missing blank line in LineDash.js.

--- Examples/Enumerations/ChartType.js
+++ Examples/Enumerations/ChartType.js
@@ -5,4 +5,3 @@
 // The resulting chart will have a "bar3D" type.
 
 var chart = Api.CreateChart("bar3D", [[200, 240, 280],[250, 260, 280]], ["Projected Revenue", "Estimated Costs"], [2014, 2015, 2016], 4051300, 2347595, 24);
-var chart = worksheet.AddChart("'Sheet1'!$A$1:$D$3", true, "bar3D", 2, 100 * 36000, 70 * 36000, 0, 2 * 36000, 7, 3 * 36000);
