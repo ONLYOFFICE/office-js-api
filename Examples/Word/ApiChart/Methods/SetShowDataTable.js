19ec4bddd54e810988aa7e5d6729c8793b114244
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2026-06-18 17:19:26 +0300

    [js-api] Add example for ShowDataTable

---
+++ Examples/Word/ApiChart/Methods/SetShowDataTable.js
@@ -0,0 +1,21 @@
+// Display the data table beneath a chart in a document.
+
+// Add the source values as a grid below the plot area, including the legend keys.
+
+// Turn on the data table with legend keys for a 3D bar chart in a document.
+
+let doc = Api.GetDocument();
+let paragraph = doc.GetElement(0);
+let chart = Api.CreateChart("bar3D", [
+	[200, 240, 280],
+	[250, 260, 280]
+], ["Projected Revenue", "Estimated Costs"], [2014, 2015, 2016], 4051300, 2347595, 24);
+let fill = Api.CreateSolidFill(Api.RGB(51, 51, 51));
+chart.SetSeriesFill(fill, 0, false);
+fill = Api.CreateSolidFill(Api.RGB(255, 111, 61));
+chart.SetSeriesFill(fill, 1, false);
+chart.SetVerAxisTitle("USD In Hundred Thousands", 10);
+chart.SetHorAxisTitle("Year", 11);
+chart.SetShowDataTable(true, true);
+chart.SetTitle("Financial Overview", 13);
+paragraph.AddDrawing(chart);
