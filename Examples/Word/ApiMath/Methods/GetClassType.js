51c89bcb28de2480afcb69d18fd5568b557c7bb9
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2026-07-14 13:47:52 +0300

    [de] Add new examples for ApiMath

---
+++ Examples/Word/ApiMath/Methods/GetClassType.js
@@ -0,0 +1,14 @@
+// Identify the object type of a math equation in a document.
+
+// How do I find out what kind of object a formula is in a document?
+
+// Confirm the category a math equation belongs to in a document.
+
+let doc = Api.GetDocument();
+let paragraph = doc.GetElement(0);
+let math = Api.CreateMath("x^2 + y^2 = z^2", "unicode");
+paragraph.AddElement(math);
+let classType = math.GetClassType();
+paragraph = Api.CreateParagraph();
+paragraph.AddText("Class Type = " + classType);
+doc.Push(paragraph);
